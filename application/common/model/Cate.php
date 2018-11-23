4a8406c7436c2bba68eb1f80607ce6bea16e35e4
tsingStar <979159190@qq.com>
2018-11-23 14:44:09 +0800

    添加分类

---
+++ application/common/model/Cate.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 商品分类表
+ * Created by PhpStorm.
+ * User: tsing
+ * Date: 2018/11/22
+ * Time: 14:49
+ */
+
+namespace app\common\model;
+
+
+use think\Model;
+
+class Cate extends Model
+{
+
+    protected function initialize()
+    {
+        parent::initialize();
+    }
+    
+
+}
